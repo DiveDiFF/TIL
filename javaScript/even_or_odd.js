3dbd79a166566349da45b1d40ae7b08010980173
silveruno <silveruno123@gmail.com>
2018-06-05 19:21:01 +0900

    Add even_or_odd.js 짝수, 홀수 반환

---
+++ javaScript/even_or_odd.js
@@ -0,0 +1,20 @@
+// function evenOrOdd(num) {
+
+//   if (num % 2 === 0) {
+//     return "Even"
+//   }
+//   else {
+//     return "Odd"
+//   }
+// }
+
+function evenOrOdd(num) {
+
+   return (num % 2 === 0) ? "Even" : "Odd";
+}
+
+
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(3));
+console.log(evenOrOdd(1000));
