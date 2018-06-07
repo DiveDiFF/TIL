752ec4d9f001fd495fd82ae6d47c207bd2cb46e3
silveruno <silveruno123@gmail.com>
2018-06-07 16:57:07 +0900

    Add strToInt.js 스트링 타입을 넘버 타입으로 변경

---
+++ javaScript/strToInt.js
@@ -0,0 +1,11 @@
+function strToInt(str) {
+  return str *= 1;
+  
+}
+
+function strToInt(str) {
+  return parseInt(str);
+}
+
+console.log(strToInt('1234'))   // 1234
+console.log(strToInt('-1234')) // -1234
