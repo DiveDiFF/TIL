3dd6252ee942107185ac8663eabb61f134fda28a
silveruno <silveruno123@gmail.com>
2018-06-08 16:46:06 +0900

    Add getmax_min.js 배열의 최대값, 최소값 구하기

---
+++ javaScript/getmax_min.js
@@ -0,0 +1,9 @@
+function getMaxValueFromArray(array) {
+  return Math.max.apply(null,array);
+}
+console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
+
+function getMinValueFromArray(array) {
+  return Math.min.apply(null,array);
+}
+console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
